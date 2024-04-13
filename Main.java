bcb7a070775310bfaa089eedecd0f94954dc308e
Hamza Ben Ali <hamza.benali@yellow-it.fr>
2024-04-13 13:38:45 +0100

    Start conflic in main classwhen pull

--- Main.java
+++ Main.java
@@ -10,10 +10,12 @@ public class Main {
 
         User user1 = new User("Mohamed" , 7 );
         User user2 = new User("Hamza" , 8 );
+        User user4 = new User("Sana"  , 10 );
 
         List<User> userList = new ArrayList<>();
         userList.add(user1);
         userList.add(user2);
+        userList.add(user4);
         GenericHelper.showDetails(userList);
     }
 }
