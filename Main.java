fa4d92e336c0354ff1bbb7c51072850498a60ca3
Hamza Ben Ali <hamza.benali@yellow-it.fr>
2024-04-13 13:16:42 +0100

    Feature : add new main method for Java entry Point

---
+++ Main.java
@@ -0,0 +1,19 @@
+import helper.GenericHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        User user1 = new User("Mohamed" , 7 );
+        User user2 = new User("Hamza" , 8 );
+
+        List<User> userList = new ArrayList<>();
+        userList.add(user1);
+        userList.add(user2);
+        GenericHelper.showDetails(userList);
+    }
+}
