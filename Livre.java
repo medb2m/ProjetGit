4cacc31eaa2db3e9323b399221095121db41dfc7
oussamadridii <oussama.titi.dridi.92@gmail.com>
2024-04-13 14:30:51 +0100

    modification

--- Livre.java
+++ Livre.java
@@ -6,6 +6,7 @@ public class Livre {
     public Livre(String titre, String auteur , int agee, int id) {
         this.titre = titre;
         this.auteur = auteur;
+        bbbb
     }
 
     // Getters et Setters
