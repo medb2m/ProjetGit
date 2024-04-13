19e61a272b7f38fd08a05c02b136f363cd12c90b
Hamza Ben Ali <hamza.benali@yellow-it.fr>
2024-04-13 13:50:59 +0100

    start conflic when mergeon master

--- Main.java
+++ Main.java
@@ -12,6 +12,7 @@ public class Main {
         User user2 = new User("Hamza" , 8 );
         User user3 = new User("asmma" , 8 );
         User user4 = new User("Sana"  , 10 );
+        User user5 = new User("toDelete "  , 10 );
 
         List<User> userList = new ArrayList<>();
         userList.add(user1);
